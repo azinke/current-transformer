7f18a5d972559f3dfdb34dd582ac5568b9a55096
AMOUSSOU Z. Kenneth <kenneth.amoussou@gmail.com>
2018-12-16 15:40:08 +0100

    Fix #1 by writing the core of the library

---
+++ CT.cpp
@@ -0,0 +1,125 @@
+/**
+ * @author: AMOUSSOU Zinsou Kenneth
+ * @date: 16-12-2018
+ * @version: 1.0
+ */
+#include "CT.h"
+
+/**
+ *  @summary: constructor of the library
+ */
+CT::CT(uint16_t turns, uint16_t shunt ,uint8_t calibre, uint8_t pin){
+    _n = turns;
+    _calibre = calibre;
+    _input = pin;
+    _shunt = shunt;
+    _offset = 512.0;    // default offset value
+}
+
+/**
+ *  @function: calibrate
+ *  @summary: do the calibration job of the sensor
+ *  @parameters: none
+ *  @return: 
+ *      float: offset
+ *  @requirement: the sensor should not be in-circuit during 
+ *                calibration
+ */
+float CT::calibrate(){
+    uint16_t i = 0;
+    float _buffer = 0;
+    for(i = 0; i <= 200; i++){
+        _buffer += analogRead(_input);
+        delayMicroseconds(10);
+    }
+    _offset = _buffer/i;
+    return _offset;
+}
+
+/**
+ *  @function: getRawData
+ *  @summary: read raw data from the sensor
+ *  @parameters: none
+ *  @return: 
+ *      float: data
+ *  @profiling:
+ *      duration: ~1ms
+ */
+float CT::getRawData(){
+    uint8_t i = 0;
+    float _buffer = 0;
+    for(i = 0; i < 100; i++){
+        _buffer += analogRead(_input);
+        delayMicroseconds(10);
+    }
+    return _buffer/i;
+}
+
+/**
+ *  @function: read
+ *  @summary: read root-Mean-Square data from the sensor
+ *  @parameters:
+ *      uint: the unit of the returned current
+ *  @return: 
+ *      float: RMS value
+ *  @profiling:
+ *      duration: ~100ms
+ */
+float CT::read(uint8_t unit){
+    uint8_t i = 0;
+    float minValue = _offset;
+    float maxValue = _offset;
+    float _buffer = 0;
+    long start = millis();
+    while(millis() - start < 100){
+        _buffer = getRawData();
+        if(_buffer > maxValue) maxValue = _buffer;
+        if(_buffer < minValue) minValue = _buffer;
+    }
+
+    if(unit == MAX) return maxValue;
+    if(unit == MIN) return minValue;
+
+    _buffer = (maxValue - minValue)/2.0;
+    _buffer = _buffer * VREF/1023.0;    // voltage
+    _buffer /= _shunt;  // compute current through the shunt
+    _buffer *= 0.70711; // RMS current through the shunt
+    _buffer *= _n;
+    if(unit == mA) return _buffer * 1000.0;
+    else return _buffer;
+}
+
+/**
+ *  @function: getMax
+ *  @summary: read the max value of the current
+ *  @parameters: none
+ *  @return: 
+ *      float: max value
+ */
+float CT::getMax(){
+    return read(MAX);    
+}
+
+/**
+ *  @function: getMin
+ *  @summary: read the min value of the current
+ *  @parameters: none
+ *  @return: 
+ *      float: min value
+ */
+float CT::getMin(){
+    return read(MIN);    
+}
+
+
+/**
+ *  @function: getOffset
+ *  @summary: read the offset value of the sensor
+ *  @parameters: none
+ *  @return: 
+ *      float: offset value
+ */
+float CT::getOffset(){
+    return _offset;
+}
+
---
+++ CT.h
@@ -0,0 +1,43 @@
+/**
+ *  @summary: current transformers sensor library
+ *  @author: AMOUSSOU Zinsou Kenneth
+ *  @date: 16-12-2018
+ *  @version: 1.0
+ */
+#ifndef H_CT
+#define H_CT
+
+#if ARDUINO < 100
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
+
+// units
+#define A   1
+#define mA  2
+#define MAX 3
+#define MIN 4
+
+// TODO: Add a method to set the reference voltage
+#define VREF    5
+
+
+class CT{
+    public:
+        CT(uint16_t turns, uint16_t shunt, uint8_t calibre, uint8_t pin=A0);
+        float getRawData();     // read raw data out of the sensor
+        float read(uint8_t uint); // read Root Mean Square data
+        float getMax();
+        float getMin();
+        float calibrate(); 
+        float getOffset();
+    private:
+        float _offset;      // offset of the ciruit with no-current
+        uint8_t _calibre;   // the sensor's caliber/gauge
+        uint16_t _n;        // number of turns
+        uint16_t _shunt;    // shunt resistor's value
+        uint8_t _input;      // input pin the sensor is connected on
+};
+
+#endif
